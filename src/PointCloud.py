8bd922d442e65ce3df2b0861980cc3de0b9a652c
Andre Cleaver <andre.cleaver@tufts.edu>
2022-10-22 14:07:27 -0400

    Created ros subscriber for point cloud

---
+++ src/PointCloud.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from sensor_msgs.msg import PointCloud2
+
+import rospy
+
+class messageConverter:
+    
+    def __init__(self):
+        rospy.init_node('Hologram_PointCloud', anonymous = True)
+        self.pointcloud2_sub = rospy.Subscriber("/camera/depth/color/points", PointCloud2, self.pointcloud_callback)
+        self.pointcloud2_pub = rospy.Publisher("/hologram/pointcloud2", PointCloud2, queue_size=1)
+        rospy.spin()
+        
+    def pointcloud_callback(self, data):
+        pass
+        # print(data)
+
+if __name__ == '__main__':
+    converter = messageConverter()
